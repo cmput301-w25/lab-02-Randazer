cd82239f6bafb4524d0e6b9546b3762ee5c2e82d
Shubham <jain8@ualberta.ca>
2025-01-16 18:21:27 -0700

    lab2

---
+++ code/app/src/main/java/com/example/lab2/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.lab2;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    ArrayAdapter<String> cityadapter;
+    ArrayList<String> dataList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        EdgeToEdge.enable(this);
+//
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+
+        cityList = findViewById(R.id.city_list);
+
+        String[] cities = {"Edmonton","London", "Paris","Ottawa"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityadapter = new ArrayAdapter<>(this,R.layout.content,dataList);
+        cityList.setAdapter(cityadapter);
+
+    }
+}
